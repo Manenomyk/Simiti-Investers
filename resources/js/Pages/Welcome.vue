85245d71b4db78445cac379870b1ed02991d3613
Manenomyk <manenomichael99@gmail.com>
2024-02-16 22:35:48 +0300

    video page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -57,10 +57,37 @@ defineProps({
                         <div class="w-5 h-1 bg-gray-600"></div>
                         <div class="absolute top-0 -right-full opacity-0 h-screen w-8/12 bg-white border transform group-focus:right-0 group-focus:opacity-100 transition-all duration-300">
                             <ul class="flex flex-col items-center w-full text-base cursor-pointer pt-10">
-                                <li class="px-4 py-1 hover:bg-gray-100 border-b">Featured Suppliers </li>
-                                <li class="px-4 py-1 hover:bg-gray-100 border-b">Search By Category</li>
-                                <li class="px-4 py-1 hover:bg-gray-100 border-b">Search By Region</li>
+                                <li class="bg-green-800 rounded-lg text-white py-2 px-6">Home</li>
+                        <li class="hover:bg-green-800 hover:rounded-lg hover:text-white py-2 px-6">About</li>
+                        <li class="hover:bg-green-800 hover:rounded-lg hover:text-white py-2 px-6">
+                            <button class="relative flex justify-center items-center gap-5 pt-0 bg-gray-100 border focus:outline-none shadow text-gray-600 rounded focus:ring ring-gray-800 hover:rounded-lg group">
+                <p class="px-4">Find suppliers</p>
+                <span class="border-l p-2 hover:bg-gray-100"><img src="../../Assets/arrow.png" class="w-3" alt=""></span>
+                <div class="fixed hidden group-focus:block z-20 top-full min-w-full w-max bg-white shadow-md mt-1 rounded">
+                    <ul class="text-left border rounded">
+                        <li class="px-4 py-1 hover:bg-gray-100 border-b">Featured Suppliers </li>
+                        <li class="px-4 py-1 hover:bg-gray-100 border-b">Search By Category</li>
+                        <li class="px-4 py-1 hover:bg-gray-100 border-b">Search By Region</li>
+                    </ul>
+                  </div>
+                </button>
+                        </li>
+                        <li class="hover:bg-green-800 hover:rounded-lg hover:text-white py-2 px-6">Contact</li>
+                        
+                        <li>
+                            <div v-if="canLogin" class=" sm:top-0 sm:end-0 p-6 text-end z-10">
+                        
+                        
+                        <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-boldbold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-100">Dashboard</Link>
+
+                    <template v-else>
+                        <Link :href="route('login')" class="hover:bg-green-800 hover:rounded-lg hover:text-white py-2 px-6 font-bold text-black hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-100">Log in</Link>
+
+                        <Link v-if="canRegister" :href="route('register')" class="ms-4 font-bold hover:bg-green-800 hover:rounded-lg hover:text-white py-2 px-6 text-black hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-blue-100">Register</Link>
+                    </template>
                     
+                </div>
+                        </li>
                             </ul>
                         </div>
 
@@ -165,6 +192,7 @@ defineProps({
                     </div>
                 </div>
 
+                <div class=""></div>
             
         </div>
 </template>
