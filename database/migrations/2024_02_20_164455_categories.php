60c9fe53fec3acf5eef14c633a3df13a04b8519f
Manenomyk <manenomichael99@gmail.com>
2024-02-20 19:47:47 +0300

    new updates

--- database/migrations/2024_02_20_164455_categories.php
+++ database/migrations/2024_02_20_164455_categories.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('Categories', function(Blueprint $table){
+            $table->id();
+            $table->string('categories-name');
+            $table->string('sub-categories');
+            $table->timestamps();
+
+        });
     }
 
     /**
