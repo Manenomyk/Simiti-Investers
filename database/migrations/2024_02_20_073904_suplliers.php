027388e1590f5dbc66142df038e9335f4015b358
Manenomyk <manenomichael99@gmail.com>
2024-02-20 10:54:46 +0300

    suppliers table migration

--- database/migrations/2024_02_20_073904_suplliers.php
+++ database/migrations/2024_02_20_073904_suplliers.php
@@ -11,7 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('Suppliers', function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->string('gender');
+            $table->string('location');
+            $table->string('region');
+            $table->string('phone');
+            $table->string('category');
+            $table->string('sub-category');
+            $table->string('annual-turnover');
+            $table->boolean('boost');
+            $table->boolean('comment');
+            $table->timestamps();
+
+        });
     }
 
     /**
