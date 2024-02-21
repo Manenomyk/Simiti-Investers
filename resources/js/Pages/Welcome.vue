6af9efcf5e20c2b179158c4d5bd47842c8defcad
Manenomyk <manenomichael99@gmail.com>
2024-02-21 14:09:31 +0300

    register business modal input

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -421,7 +421,17 @@ defineProps({
                                         x
                                     </button>
                                 </div>
-                                <div class="md:flex gap-3">
+                                <div class="md:flex gap-3 mb-5">
+                                    <div class="flex flex-col">
+                                        <span>Name</span>
+                                        <input class="rounded" type="text">
+                                    </div>
+                                    <div class="flex flex-col">
+                                        <span>Email</span>
+                                        <input class="rounded" type="text">
+                                    </div>
+                                </div>
+                                <div class="md:flex gap-3 mb-5">
                                     <div class="flex flex-col">
                                         <span>Name</span>
                                         <input class="rounded" type="text">
