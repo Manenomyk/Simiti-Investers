9b5a6a5434f7fcecbadf3a29789bced289ac1e4f
Manenomyk <manenomichael99@gmail.com>
2024-03-07 14:49:13 +0300

    dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,19 +4,71 @@ import Welcome from '@/Components/Welcome.vue';
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
+<AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Accountant</h2>
         </template>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
+        <div>
+            <div class="outercard md:flex-row flex-col mt-4">
+                <div class="flex">
+                    <div class="card1 mx-auto mb-3 mr-[1rem]">
+                        <div class="text-center mt-2">
+                            <p class="cardhead">
+                                <i>Total Users</i>
+                            </p>
+                        </div>
+                        <div class="">
+                            <p>
+                                All Users:
+                            </p>
+                            <h3>users</h3>
+                        </div>
+                    </div>
+                    <div class="card2 mx-auto mb-3">
+                        <div class="text-center mt-2">
+                            <p>
+                                <i>Total Vehicles</i>
+                            </p>
+                        </div>
+                        <div class="">
+                            <p>
+                                All Vehicles:
+                            </p>
+                            <h3>trucks</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="flex">
+                    <div class="card3 mx-auto mb-3 mr-[1rem]">
+                        <div class="text-center mt-2">
+                            <p>
+                                <i>Total Customers</i>
+                            </p>
+                        </div>
+                        <div class="">
+                            <p>
+                                Customers:
+                            </p>
+                            <h3>customers</h3>
+                        </div>
+                    </div>
+                    <div class="card4 mx-auto mb-3">
+                        <div class="text-center mt-2">
+                            <p>
+                                <i>Total Sales</i>
+                            </p>
+                        </div>
+                        <div class="">
+                            <p>
+                                Sales margin:
+                            </p>
+                            <h3>profitx</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <canvas id="first"></canvas>
         </div>
+
     </AppLayout>
 </template>
