efbe8e1c746480a8bcaac3298991eaae1a7fdfde
Manenomyk <manenomichael99@gmail.com>
2024-03-12 13:00:00 +0300

    blogs page creation

---
+++ resources/js/Pages/Blogs.vue
@@ -0,0 +1,19 @@
+<script>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Sidebar from "../Components/Sidebar.vue";
+export default {
+components: {Sidebar, AppLayout }
+}
+</script>
+
+<template>
+  <div>
+    <AppLayout title="Dashboard" class="flex-1">
+        <div class="flex min-h-screen">
+            <Sidebar />
+        </div>
+    </AppLayout>
+  </div>
+</template>
+
+
