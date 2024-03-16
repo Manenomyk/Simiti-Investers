9a855d712e47ec7c2e4fc8250b6de53400b3a9bf
Manenomyk <manenomichael99@gmail.com>
2024-03-17 02:36:35 +0300

    controller details

--- app/Http/Controllers/InsurerController.php
+++ app/Http/Controllers/InsurerController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use Illuminate\Http\Request;
+use App\Models\InsuranceServices;
 
 class InsurerController extends Controller
 {
-    //
+    public function index( ){
+        $data = InsuranceServices::all();
+        return Inertia::render('Categories', compact('data'));
+    }
 }
--- app/Http/Controllers/LogisticsController.php
+++ app/Http/Controllers/LogisticsController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use Illuminate\Http\Request;
+use App\Models\LogisticServices;
 
 class LogisticsController extends Controller
 {
-    //
+    public function index( ){
+        $data = LogisticServices::all();
+        return Inertia::render('Categories', compact('data'));
+    }
 }
