4ccb87992e706347830808616fe0a44bd8ac708e
Manenomyk <manenomichael99@gmail.com>
2024-03-17 01:09:43 +0300

    insurance resource info

--- app/Filament/Resources/InsuranceServicesResource.php
+++ app/Filament/Resources/InsuranceServicesResource.php
@@ -2,16 +2,18 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\InsuranceServicesResource\Pages;
-use App\Filament\Resources\InsuranceServicesResource\RelationManagers;
-use App\Models\InsuranceServices;
 use Filament\Forms;
-use Filament\Resources\Form;
-use Filament\Resources\Resource;
-use Filament\Resources\Table;
 use Filament\Tables;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use App\Models\InsuranceServices;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\FileUpload;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\InsuranceServicesResource\Pages;
+use App\Filament\Resources\InsuranceServicesResource\RelationManagers;
 
 class InsuranceServicesResource extends Resource
 {
@@ -22,16 +24,50 @@ class InsuranceServicesResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
+        ->schema([
+            Forms\Components\Section::make('')
+            ->description('Enter Logistic Services Details')
             ->schema([
-                //
-            ]);
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('email')->email()->required(),
+                Forms\Components\TextInput::make('phone')->required(),
+            
+            Forms\Components\TextInput::make('comment')->required(),
+            Radio::make('gender')->required()
+            ->options([
+                'Male' => 'Male',
+                'Female' => 'Female',
+            ]),
+            ])->columns(3),
+
+            Forms\Components\Section::make('')
+            ->description('Logistic Services Location Details')
+            ->schema([
+                Forms\Components\TextInput::make('location')->required(),
+                Forms\Components\TextInput::make('region')->required(),
+            ])->columns(2),
+
+
+            Forms\Components\Section::make('')
+            ->description('Logistic Services Uploads')
+            ->schema([
+                FileUpload::make('photo'),
+        
+            ])->columns(3),
+            
+
+        ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('phone'),
+                Tables\Columns\TextColumn::make('location'),
+                Tables\Columns\TextColumn::make('region'),
             ])
             ->filters([
                 //
