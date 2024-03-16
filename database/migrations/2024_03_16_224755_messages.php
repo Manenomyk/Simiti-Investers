4bacedeb4882a87f86d160f47f09bc0f363b5a0e
Manenomyk <manenomichael99@gmail.com>
2024-03-17 01:51:42 +0300

    messages model migration info

--- database/migrations/2024_03_16_224755_messages.php
+++ database/migrations/2024_03_16_224755_messages.php
@@ -11,7 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('Messages', function(Blueprint $table){
+            $table->id();
+            $table->string('user_name');
+            $table->string('email');
+            $table->string('message');
+            $table->timestamps();
+
+        });
     }
 
     /**
