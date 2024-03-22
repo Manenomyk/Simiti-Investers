2f3682ea4f4ed360ae48ae69580f2979fb5d984a
Manenomyk <manenomichael99@gmail.com>
2024-03-22 22:43:28 +0300

    about page finals

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -21,7 +21,7 @@ import "../../css/app.css";
                 <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
                     <img
                         className="w-[300px] mx-auto my-3"
-                        src="../../Assets/log.png"
+                        src="../../Assets/deal.svg"
                         alt="logo images"
                     />
                     <div className="flex flex-col justify-center">
@@ -34,7 +34,7 @@ import "../../css/app.css";
                         <p
                             className="md:text-2xl sm:text-xl text-lg font-bold py-2"
                         >
-                        Building Connections, Constructing Futures
+                            Building Connections, Constructing Futures
                         </p>
 
                         <p>
@@ -49,6 +49,44 @@ import "../../css/app.css";
                             project at a time.
                         </p>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div className="w-full p-[5rem] px-4 aboutHouse bg-cover bg-center">
+                <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+                    <div className="flex flex-col justify-center">
+                        <!-- <h1
+                            className="w-full text-3xl font-bold text-yellow-500"
+                        >
+                            Simiti Constructors
+                        </h1> -->
+
+                        <p
+                            className="md:text-2xl sm:text-xl text-lg text-white font-bold py-2"
+                        >
+                            What Sets us apart
+                        </p>
+
+                        <p>
+                            Simiti is your gateway to a seamless construction
+                            experience. As a premier construction listing
+                            website, we connect contractors, builders, and
+                            suppliers, making it effortless to find the right
+                            expertise and resources for your projects. Our
+                            platform offers a comprehensive directory, project
+                            showcases, and collaboration tools, empowering you
+                            to bring your construction visions to life. With
+                            Simiti, every step of your construction journey is
+                            simplified and enhanced, ensuring success and
+                            satisfaction every time
+                        </p>
+                    </div>
+                    <img
+                        className="w-[300px] mx-auto my-3"
+                        src="../../Assets/log.png"
+                        alt="logo images"
+                    />
                 </div>
             </div>
         </div>
