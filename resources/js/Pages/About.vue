df97c52953e1a3f3814365a06e71c9cee4ba23ca
Manenomyk <manenomichael99@gmail.com>
2024-03-22 16:34:24 +0300

    about page

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,13 +1,28 @@
 <script setup>
 import Footer from "@/Components/Footer.vue";
 import TopNav from "../Components/TopNav.vue";
-
+import "../../css/app.css";
 </script>
 
 <template>
-  <div>
-    <TopNav />
-  </div>
+    <div>
+        <TopNav />
+        <div className="about_background w-full h-[30rem] mt-18 bg-cover bg-center">
+            <h1
+                className="w-full text-3xl font-bold text-[#e4c131] text-end absolute top-[25rem] pr-6 "
+            >
+                Nurturing Visions, Building Legacies
+            </h1>
+        </div>
+        <div>
+            <div>
+                <div
+                    className="max-w-[1240px] mx-auto mt-10 gap-8 text-gray-300 mb-10"
+                ></div>
+            </div>
+        </div>
+        <div>
+            <Footer />
+        </div>
+    </div>
 </template>
-
-
