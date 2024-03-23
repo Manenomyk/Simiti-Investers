9071f624de69ae4ba0de0b9f4b57e04daf1f1ed3
Manenomyk <manenomichael99@gmail.com>
2024-03-23 13:56:22 +0300

    post sample

--- resources/js/Pages/Blogs.vue
+++ resources/js/Pages/Blogs.vue
@@ -18,9 +18,50 @@ export default {
                         className="max-w-[1240px] mx-auto mx-3 grid md:grid-cols-4 gap-2 text-gray-300"
                     >
                         <div
-                            className=" container md:col-span-3 rounded-md shadow-lg bg-white flex justify-between mt-10"
-                        ></div>
-                        <div
+                            className=" container md:col-span-4 rounded-md shadow-lg bg-white flex justify-between mt-10"
+                        >
+                            <div
+                                class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-10 border-b border-gray-200"
+                            >
+                                <div>
+                                    <img src="../../Assets/pic.svg" alt="" />
+                                </div>
+                                <div>
+                                    <h2
+                                        class="text-gray-700 font-bold text-4xl pb-4"
+                                    >
+                                        My amazing blog post
+                                    </h2>
+
+                                    <span class="text-gray-500">
+                                        By
+                                        <span
+                                            class="font-bold italic text-gray-800"
+                                            >Amazing Bahati</span
+                                        >, daily routine
+                                    </span>
+
+                                    <p
+                                        class="text-md text-gray-700 pt-8 pb-5 leading-8 font-light"
+                                    >
+                                        Lorem ipsum dolor, sit amet consectetur
+                                        adipisicing elit. Culpa obcaecati,
+                                        facilis quis quas beatae asperiores,
+                                        dignissimos, dolore nostrum rem minus
+                                        quo dolorum. Voluptates, libero
+                                        accusamus assumenda nobis repellat
+                                        numquam suscipit.
+                                    </p>
+
+                                    <a
+                                        href=""
+                                        class=" bg-green-800 text-white font-extrabold py-2 m-2 px-6 rounded-lg"
+                                        >Read more</a
+                                    >
+                                </div>
+                            </div>
+                        </div>
+                        <!-- <div
                             class="mt-10 rounded-md shadow-lg sm:items-center text-black"
                         >
                             <div class=" ">
@@ -50,7 +91,7 @@ export default {
                                     </div>
                                 </a>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
