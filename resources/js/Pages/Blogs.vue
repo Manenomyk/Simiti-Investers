19f48473051b99203698e685606bee20a4fcc28b
Manenomyk <manenomichael99@gmail.com>
2024-03-25 02:32:24 +0300

    sidebar responsiveness

--- resources/js/Pages/Blogs.vue
+++ resources/js/Pages/Blogs.vue
@@ -14,25 +14,25 @@ defineProps({
                 <div class="min-h-screen">
                     <Sidebar />
                 </div>
-                <div class="bg-gray-100">
+                <div class="bg-gray-100 mx-4">
                     <!-- {{ data }} -->
                     <div
                         v-for="item in data"
                         :key="item.id"
-                        className="max-w-[1240px] mx-auto mx-3 grid md:grid-cols-4 gap-2 text-gray-300"
+                        className="max-w-[1240px] mx-auto mx-3 grid md:grid-cols gap-2 text-gray-300"
                     >
                         <div
-                            className=" container md:col-span-4 rounded-md shadow-lg bg-white flex justify-between mt-10"
+                            className=" container rounded-md shadow-lg bg-white flex justify-between mt-10"
                         >
                             <div
-                                class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-10 border-b border-gray-200"
+                                class="sm:grid grid-cols-2 gap-20 w-full mx-auto py-10 border-b border-gray-200"
                             >
                                 <div>
                                     <img src="../../Assets/pic.svg" alt="" />
                                 </div>
                                 <div>
                                     <h2
-                                        class="text-gray-700 font-bold text-4xl pb-4"
+                                        class="text-gray-700 font-bold text-3xl pb-4"
                                     >
                                         {{ item.title }}
                                     </h2>
