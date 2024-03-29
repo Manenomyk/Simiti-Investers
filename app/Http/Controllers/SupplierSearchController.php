9deae461665d0ffe4dd0113d2c4a1ce77986dd9d
Manenomyk <manenomichael99@gmail.com>
2024-03-29 10:51:47 +0300

    search controller

--- app/Http/Controllers/SupplierSearchController.php
+++ app/Http/Controllers/SupplierSearchController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SupplierSearch;
 use Illuminate\Http\Request;
 
 class SupplierSearchController extends Controller
 {
-    //
+    public function index(Request $request){
+        $report = SupplierSearch::query()
+            ->when($request->input('search'), function ($query, $search) {
+                $query->where('category', 'LIKE', "%{$search}%");
+            });
+    }
 }
