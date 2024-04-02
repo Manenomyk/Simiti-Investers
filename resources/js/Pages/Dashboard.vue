9466149808bdf911fb2884bc7929c8aff190cfcc
Manenomyk <manenomichael99@gmail.com>
2024-04-03 02:10:50 +0300

    dashboard changes

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,52 +14,52 @@ import Sidebar from "../Components/Sidebar.vue";
                 <div class="min-h-screen">
                     <Sidebar />
                 </div>
-                <div class="w-screen  ">
+                <div class="w-screen my-5  ">
                     <div class="flex gap-10 justify-center">
                         <div class="card1 mx-auto mb-3 ">
                             <div class="text-center mt-2">
                                 <p class="cardhead">
-                                    <i>Total Users</i>
+                                    <i>Total Suppliers</i>
                                 </p>
                             </div>
                             <div class="">
-                                <p>All Users:</p>
-                                <h3>users</h3>
+                                <p class="text-3xl">154</p>
+                                
                             </div>
                         </div>
                         <div class="card2 mx-auto mb-3">
-                            <div class="text-center mt-2">
+                            <div class="cardhead text-center mt-2">
                                 <p>
-                                    <i>Total Vehicles</i>
+                                    <i>Total Categories</i>
                                 </p>
                             </div>
                             <div class="">
-                                <p>All Vehicles:</p>
-                                <h3>trucks</h3>
+                                <p class="text-4xl">8</p>
+                               
                             </div>
                         </div>
                     
                     
                         <div class="card3 mx-auto mb-3 ">
-                            <div class="text-center mt-2">
+                            <div class="cardhead text-center mt-2">
                                 <p>
-                                    <i>Total Customers</i>
+                                    <i>Total Sub-Categories</i>
                                 </p>
                             </div>
                             <div class="">
-                                <p>Customers:</p>
-                                <h3>customers</h3>
+                                <p class="text-3xl">12</p>
+                                <!-- <h3>customers</h3> -->
                             </div>
                         </div>
                         <div class="card4 mx-auto mb-3">
-                            <div class="text-center mt-2">
+                            <div class="cardhead text-center mt-2">
                                 <p>
-                                    <i>Total Sales</i>
+                                    <i>Total Blogs</i>
                                 </p>
                             </div>
                             <div class="">
-                                <p>Sales margin:</p>
-                                <h3>profitx</h3>
+                                <p class="text-3xl">6</p>
+                                <!-- <h3>profitx</h3> -->
                             </div>
                         </div>
                     </div>
