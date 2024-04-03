de091749a412d4de212b94ae2eb3ea8c37e0df01
Manenomyk <manenomichael99@gmail.com>
2024-04-03 13:32:34 +0300

    search bar code

---
+++ resources/js/Pages/SearchBar.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Footer from "@/Components/Footer.vue";
+import TopNav from "../Components/TopNav.vue";
+
+defineProps({
+    data: Object,
+});
+
+</script>
+<template>
+    <div>
+        <TopNav />
+        <div className="about_background w-full h-[30rem] mt-18 bg-cover bg-center">
+            <h1 className="w-full text-3xl font-bold text-[#e4c131] text-end absolute top-[25rem] pr-6 ">
+                Explore a world of posibilities
+            </h1>
+        </div>
+    </div>
+</template>
