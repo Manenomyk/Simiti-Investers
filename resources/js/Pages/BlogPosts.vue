d41c96d39f62d1946f96b381a5e36685ee54c982
Manenomyk <manenomichael99@gmail.com>
2024-04-03 13:11:48 +0300

    blogposts page updates

--- resources/js/Pages/BlogPosts.vue
+++ resources/js/Pages/BlogPosts.vue
@@ -1,9 +1,17 @@
 <script setup>
 import Footer from "@/Components/Footer.vue";
 import TopNav from "../Components/TopNav.vue";
+import { computed } from "vue";
 
 defineProps({
     data: Object,
+});
+const computedImagePath = computed(() => (image) => {
+    if (!image || !image) {
+        console.error("Image or image name is missing:", image);
+        return ""; // Or provide a default path
+    }
+    return `/storage/${image}`;
 });
 </script>
 
@@ -11,41 +19,27 @@ defineProps({
     <div>
         <TopNav />
         <div class="bg-gray-100">
-            <div
-                v-for="item in data"
-                :key="item.id"
-                className="max-w-[1240px] mt-4 mx-auto py-10 grid md:grid-cols-5 gap-8 text-gray-300 mb-10"
-            >
-                <div
-                    class="mt-10 rounded-md shadow-lg sm:items-center text-black"
-                >
+            <div v-for="item in data" :key="item.id"
+                className="max-w-[1240px] mt-4 mx-auto py-10 grid md:grid-cols-5 gap-8 text-gray-300 mb-10">
+                <div class="mt-10 rounded-md shadow-lg sm:items-center text-black">
                     <div class=" ">
-                        <img
-                            src="../../Assets/pic.svg"
-                            class="rounded-full object-cover mb-5 h-[150px]"
-                            alt="logo image"
-                        />
-                        <div
-                            class="mt-4 mb-4 font-bold text-green-800 m-3 text-lg"
-                        >
+                        <img :src="computedImagePath(item.profile)" class="rounded-full object-cover mb-5 h-[150px]"
+                            alt="logo image" />
+                        <div class="mt-4 mb-4 font-bold text-green-800 m-3 text-lg">
                             About the Owner
                         </div>
                         <div class="text-sm m-2">
                             {{ item.about }}
                         </div>
-                        <div
-                            class="bg-[#ecba00] rounded-lg w-[150px] text-black py-1 m-2 px-6"
-                        >
+                        <div class="bg-[#ecba00] rounded-lg w-[150px] text-black py-1 m-2 px-6">
                             Learn More
                         </div>
                     </div>
                 </div>
 
-                <div
-                    className=" container md:col-span-3 rounded-md shadow-lg bg-white p-5 mt-10"
-                >
-                    <div class="w-[400px]">
-                        <img src="../../Assets/pic.svg" alt="" />
+                <div className=" container md:col-span-3 rounded-md shadow-lg bg-white p-5 mt-10">
+                    <div class="w-[400px] flex justify-center">
+                        <img :src="computedImagePath(item.image1)" alt="" />
                     </div>
                     <div>
                         <h2 class="text-gray-700 font-bold text-3xl mt-4 pb-4">
@@ -54,27 +48,18 @@ defineProps({
 
                         <span class="text-gray-500">
                             By
-                            <span class="font-bold italic text-gray-800"
-                                >Amazing Bahati</span
-                            >, Created on {{ item.updated_at }}
+                            <span class="font-bold italic text-gray-800">Amazing Bahati</span>, Created on {{
+                item.updated_at }}
                         </span>
 
-                        <p
-                            class="text-md text-gray-700 pt-8 pb-5 leading-8 font-light"
-                        >
+                        <p class="text-md text-gray-700 pt-8 pb-5 leading-8 font-light">
                             {{ item.content1 }}
                         </p>
 
-                        <a
-                            href=""
-                            class="bg-green-800 text-white font-extrabold py-2 m-2 px-6 rounded-lg"
-                            >Read more</a
-                        >
+                        <a href="" class="bg-green-800 text-white font-extrabold py-2 m-2 px-6 rounded-lg">Read more</a>
                     </div>
                 </div>
-                <div
-                    class="mt-10 rounded-md shadow-lg sm:items-center bg-white text-black"
-                >
+                <div class="mt-10 rounded-md shadow-lg sm:items-center bg-white text-black">
                     <div class="m-2">
                         <h2 className="text-green-800 font-bold text-xl mb-4">
                             Contacts
@@ -93,9 +78,7 @@ defineProps({
                                 Nairobi.
                             </li>
                         </ul>
-                        <div
-                            class="bg-green-800 rounded-lg w-[150px] text-white py-1 m-2 px-6"
-                        >
+                        <div class="bg-green-800 rounded-lg w-[150px] text-white py-1 m-2 px-6">
                             Message us
                         </div>
                     </div>
